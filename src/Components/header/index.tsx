9af51568dc69868de02e8dcd31c28cdd3c6881f3
Cintia Marques <cintiamarques.mk@gmail.com>
2023-10-14 18:33:46 -0300

    component header

---
+++ src/Components/header/index.tsx
@@ -0,0 +1,11 @@
+import jornal from '../../image/jornal.png'
+function Header() {
+  return(
+    <>
+    <img src={ jornal } alt="desenho-de-jornal" />
+    <h1>Notícias IBGE Online</h1>
+    </>
+  )
+}
+
+export default Header; 
