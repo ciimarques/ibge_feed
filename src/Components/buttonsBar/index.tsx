22736ace9acc7c09f6e2c1b9d7ae70d7366a9e72
Cintia Marques <cintiamarques.mk@gmail.com>
2023-10-20 18:35:06 -0300

    tailwind buttonsBar

--- src/Components/buttonsBar/index.tsx
+++ src/Components/buttonsBar/index.tsx
@@ -5,21 +5,29 @@ function ButtonsBar() {
   return (
     <div>
       <button
+        className="font-bold px-4 py-3
+        hover:text-indigo-500 hover: transition duration-300"
         onClick={ () => setFilterType(null) }
       >
         Mais Recente
       </button>
       <button
+        className="font-bold px-4 py-3
+        hover:text-indigo-500 hover: transition duration-300"
         onClick={ () => { setFilterType('Release'); setFilterFavorites(null); } }
       >
         Release
       </button>
       <button
+        className="font-bold px-4 py-3
+        hover:text-indigo-500 hover: transition duration-300"
         onClick={ () => { setFilterType('Notícia'); setFilterFavorites(null); } }
       >
         Notícia
       </button>
       <button
+        className="font-bold px-4 py-3
+        hover:text-indigo-500 hover: transition duration-300"
         onClick={ () => { setFilterType(null); setFilterFavorites(true); } }
       >
         Favoritos
