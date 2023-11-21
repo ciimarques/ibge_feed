92f4bdc5d1c19a7fee54a623b676827b12200851
Cintia Marques <cintiamarques.mk@gmail.com>
2023-11-21 18:09:47 -0300

    corrigindo estilização componente SearchBar

--- src/Components/SearchBar/index.tsx
+++ src/Components/SearchBar/index.tsx
@@ -3,7 +3,10 @@ import TitleFilter from '../titleFilter/index';
 
 function SearchBar() {
   return (
-    <div className="grid p-3 sm:grid-cols-1 md:grid-cols-2 w-full  shadow-lgshadow-lg">
+    <div
+      className="grid p-3 sm:grid-cols-1 md:grid-cols-2
+        justify-items:center w-full shadow-lgshadow-lg"
+    >
       <ButtonsBar />
       <TitleFilter />
     </div>
